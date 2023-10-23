cf0557ec978f56db65409a9d063aa23dba5fa31a
Mateus2222 <mateuspolari@gmail.com>
2023-10-22 23:50:53 -0300

    adicao de js

---
+++ js/main.js
@@ -0,0 +1,52 @@
+import opcoes_planos from './plans/planos.js';
+
+
+const plans_cards = document.querySelector(".pricing") 
+
+function colocar_planos(planos){
+    if(planos.dados_moveis == '' && planos.redes_sociais == ''){
+        return `        
+            <div class="pricing-plan">
+                <h2 class="pricing-plan__title">Premium</h2>
+                <p><span class="pricing-plan__price">R$ 90</span> por/mês</p>
+                <ul class="pricing-plan__benefits">
+                    <li>${planos.internet_residencial}</li>
+                    <li>${planos.tv}</li>
+                    <li>${planos.ligacoes}</li>
+                </ul>
+                <a href="#" class="pricing-plan__cta">Assine agora</a>
+            </div>`
+    }
+    else if(planos.redes_sociais == ''){
+        return `        
+            <div class="pricing-plan">
+                <h2 class="pricing-plan__title">Premium</h2>
+                <p><span class="pricing-plan__price">R$ 90</span> por/mês</p>
+                <ul class="pricing-plan__benefits">
+                    <li>${planos.internet_residencial}</li>
+                    <li>${planos.tv}</li>
+                    <li>${planos.ligacoes}</li>
+                    <li>${planos.dados_moveis}</li>
+                </ul>
+                <a href="#" class="pricing-plan__cta">Assine agora</a>
+            </div>`
+    }
+    else{
+        return `        
+            <div class="pricing-plan">
+                <h2 class="pricing-plan__title">Premium</h2>
+                <p><span class="pricing-plan__price">R$ 90</span> por/mês</p>
+                <ul class="pricing-plan__benefits">
+                    <li>${planos.internet_residencial}</li>
+                    <li>${planos.tv}</li>
+                    <li>${planos.ligacoes}</li>
+                    <li>${planos.dados_moveis}</li>
+                    <li>${planos.redes_sociais}</li>
+                </ul>
+                <a href="#" class="pricing-plan__cta">Assine agora</a>
+            </div>`
+    }
+}
+
+
+opcoes_planos.map((opcao) => plans_cards.insertAdjacentHTML('beforeend',colocar_planos(opcao)))
---
+++ js/plans/planos.js
@@ -0,0 +1,25 @@
+const opcoes_planos = [
+    {   
+        internet_residencial: '80 megas',
+        tv: 'Plano básico de TV',
+        ligacoes: '60 minutos de ligaçôes',
+        dados_moveis: '',
+        redes_sociais: ''
+    },
+    {
+        internet_residencial: '150 megas',
+        tv: 'Plano intermediário de TV',
+        ligacoes: '120 minutos de ligaçôes',
+        dados_moveis: '7gb internet móvel',
+        redes_sociais: ''
+    },
+    {
+        internet_residencial: '300 megas',
+        tv: 'Plano premium de TV',
+        ligacoes: 'ligaçôes ilimitadas',
+        dados_moveis: '15gb internet móvel',
+        redes_sociais: 'Redes sociais ilimitadas'
+    }
+]
+
+export default opcoes_planos;
